be1a37bd47db60b9426fc8e9f76638ff7592d7a2
Megarekrut65 <55782081+Megarekrut65@users.noreply.github.com>
2022-11-15 16:38:04 +0200

    Added creating splines for all options

--- control-work/main.py
+++ control-work/main.py
@@ -3,9 +3,11 @@ import json
 
 
 def main():
-    f = open('19.json')
-    data = json.load(f)
-    run_first(points=data['curve'])
+    for i in range(1, 25):
+        f = open(f'points/{i}.json')
+        data = json.load(f)
+        print(i)
+        run_first(points=data['curve'])
 
 
 if __name__ == '__main__':
